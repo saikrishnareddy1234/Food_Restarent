ae7a51d421cc174701d88695fca3fd9301221aed
koushik-123456789 <saikrishna130204@gmail.com>
2026-09-05 16:53:35 +0530

    first commit

--- src/functional_component/fetch.jsx
+++ src/functional_component/fetch.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-function Getproducts() {
+function Getproducts(){
     const[products,setproducts]=useState([])
-    async function getproducts() {
+    const get_recipes=async()=>{
         const response = await fetch('http://127.0.0.1:8000/recipes/')
-        const data = await response.json()
-        setproducts(data.recipes)
+        const products = await response.json()
+        setproducts(products.recipes)
     }
     return <div>
-        <div><button onClick={getproducts}>Get Products</button></div>
+        <div><button onClick={get_recipes}>Get Products</button></div>
         {
             products.length>0?
             products.map((each)=><img style={{width:'200px'}} src ={each.image}/>)
